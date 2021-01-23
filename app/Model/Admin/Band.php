fc93702c062da8ee37b4f44e0164038443f1bdbd
Bz-Carter <lebz2016@gmail.com>
2021-01-23 19:45:22 +0000

    commit 3

---
+++ app/Model/Admin/Band.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Band extends Model
+{
+    protected $fillable = [
+        'brand_name', 'brand_logo'
+    ];
+}
